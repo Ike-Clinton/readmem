245b410539d1d1fd3b007b796f8733180d1a6e15
Ike Clinton <IkeClinton@gmail.com>
2016-11-25 16:26:36 -0500

    Boiler Plate Visual Studio Files
    Hello world cpp

---
+++ readmem/readmem.cpp
@@ -0,0 +1,13 @@
+// readmem.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	cout << "Hello world!" << endl;
+    return 0;
+}
+
