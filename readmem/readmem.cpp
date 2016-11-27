cf2742552106e2ebdd8bb0dc30ff1c6447bf6ec2
Ike Clinton <IkeClinton@gmail.com>
2016-11-26 22:15:26 -0500

    Added boilerplate code for reading a simple table value.
    Code is currently non-working or functional other than that it compiles.

--- readmem/readmem.cpp
+++ readmem/readmem.cpp
@@ -1,63 +1,79 @@
-// readmem.cpp : Defines the entry point for the console application.
-//
-
 #include "stdafx.h"
-#include "windows.h"
-
+#include <Windows.h>
 #include <iostream>
 
 using namespace std;
 
-
-
-
 /**
- * This program gets a process handle to "Minesweeper"
- * it then prints out the value at "address" every half second
- * using ReadProcessMemory
- * This value is simply the current number of mines on the level
- */
+* This program gets a process handle to "Minesweeper"
+* it then prints out the value at "address" every half second
+* using ReadProcessMemory
+* This value is simply the current number of mines on the level
+*/
 int main()
 {
-	// The address we want to read from
-	//DWORD address = 0x100579C;
-	DWORD address = 0x01F4C380;
-	// Store the value we are reading
-	int value = 0;
+	// Offsets
+	DWORD dwImageBase = 0x400000;
+	DWORD dwVirtualAddress = 0x00001000;
+	DWORD dwBaseAddress = 0x000111;
+	//DWORD dwOldWorldBase = 0x103BBA8;
+	DWORD pWorldBase = 0x103ABF0;
+	//WORD wOldPlayerOffset = 0x1650;
+	WORD wPlayerOffset = 0x1658;
+	BYTE bVstateOffset = 0x34;
+
+	// Number of bytes read by RPM
+	DWORD dwBytesRead = 0;
+	HWND hwnd;
+	hwnd = FindWindow(NULL, L"DayZ");
+
 	// The process ID, used to r/w into the memory
 	DWORD pid;
-	// A Handle to the window
-	HWND hwnd;
-
-	hwnd = FindWindow(NULL, L"Minesweeper");
-	
-
-	// If FindWindow is NULL, exit
-	if (!hwnd)
-	{
-		cout << "Window not found, FindWindow returned NULL\n";
-		return 0;
-	}
-
 	// Get the process ID and place the value at the address of pid
 	GetWindowThreadProcessId(hwnd, &pid);
+	cout << "pid = " << pid << "\n";
+
 	// Open a handle to the process specified by pid
-	HANDLE phandle = OpenProcess(PROCESS_VM_READ, 0, pid);
+	HANDLE phandle;
 	// exit if OpenProcess returns NULL
-	if (!phandle)
+	if (!(phandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid)))
 	{
-		cout << "Could not get process handle, OpenProcess returned error\n";
+		cout << "ERROR: Could not get process handle, OpenProcess returned error\n";
 		return 0;
 	}
+
+	
+	// Follow the pointer value to the player table
+	DWORD dwAddr1;
+	DWORD dwAddr2;
+	DWORD dwAddr3;
+	dwAddr1 = ReadProcessMemory(phandle, (LPCVOID)(dwImageBase + pWorldBase), &dwAddr1, sizeof(int), &dwBytesRead);
+
+	printf("Read %d bytes\n", dwBytesRead);
+	printf("Initial Value: %010X\n", dwAddr1);
+
+	dwAddr2 = ReadProcessMemory(phandle, (LPCVOID)(dwAddr1 + wPlayerOffset), &dwAddr2, sizeof(int), &dwBytesRead);
+
+	printf("Read %d bytes\n", dwBytesRead);
+	printf("Second Value: %010X\n", dwAddr2);
 
 	// Read forever until program closes
 	while (1)
 	{
-		ReadProcessMemory(phandle, (void*)address, &value, sizeof(value), 0);
-		cout << value << "\n";
-		Sleep(500);
+		// Read the memory at the specified address, store it at &values
+		//ReadProcessMemory(phandle, (void*)dwPtrBaseAddress+dwWorldBase, &value, sizeof(int), &dwPtrBytesRead);
+		dwAddr3 = ReadProcessMemory(phandle, (LPCVOID)(dwAddr2 + bVstateOffset), &dwAddr3, sizeof(int), &dwBytesRead);
+
+		printf("Read %d bytes\n", dwBytesRead);
+		printf("Value: %010X\n", dwAddr3);
+
+		Sleep(1000);
 	}
-	
-    return 0;
+	return 0;
 }
+
+
+
+
+
 
